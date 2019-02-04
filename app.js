ebeaa76e154bde306aebd085b9aac5fa0ebbe1e2
ralexandre75 <rene.alexandre@grimoire-design.fr>
2019-02-04 15:43:17 +0100

    creation of a middleware

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ app.set('port', (process.env.port || 3000));
 
 app.use(cors());
 app.use('/api/v1', api);
+app.use((req, res) => {
+    const err = new Error('404 - Not found !!!!!');
+    err.status = 404;
+    res.json({msg: '404 - Not found !!!!!', err:err});
+})
 
 app.listen(app.get('port'), () => {
     console.log(`express server listening on port ${app.get('port')}`);
