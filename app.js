e626cf2540aa3f197b5d0cff4e8be24e1522a5b8
ralexandre75 <rene.alexandre@grimoire-design.fr>
2019-02-04 11:02:51 +0100

    use of middlewares

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
 const express = require('express');
 const app = express();
 const api = require('./api/v1/index');
+const cors = require('cors');
+
+app.set('port', (process.env.port || 3000));
+
+app.use(cors());
+app.use('./api/v1', api);
+
+app.listen(app.get('port'), () => {
+    console.log(`express server listening on port ${app.get('port')}`);
+});
