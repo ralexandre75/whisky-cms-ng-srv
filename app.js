135f4841aa557af760c684cc162cbf90f377df66
ralexandre75 <rene.alexandre@grimoire-design.fr>
2019-02-08 10:39:41 +0100

    installation and configuration of Mongoose

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const api = require('./api/v1/index');
 const cors = require('cors');
+const mongoose = require('mongoose');
+const connection = mongoose.connection;
 
 app.set('port', (process.env.port || 3000));
 
@@ -13,6 +15,16 @@ app.use((req, res) => {
     res.json({msg: '404 - Not found !!!!!', err:err});
 })
 
-app.listen(app.get('port'), () => {
-    console.log(`express server listening on port ${app.get('port')}`);
-});
+mongoose.connect('mongodb://localhost:27017/whiskycms', {useNewUrlParser: true});
+connection.on('error', (err) => {
+    console.error(`connection to MongoDB error :  ${err.message}`);
+})
+
+connection.once('open', () => {
+    console.log('connected to MongoDB');
+    app.listen(app.get('port'), () => {
+        console.log(`express server listening on port ${app.get('port')}`);
+    });
+})
+
+
