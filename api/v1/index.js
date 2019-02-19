eade8474d02fa7b413de7d4d124fa29eec2eaa5f
ralexandre75 <rene.alexandre@grimoire-design.fr>
2019-02-19 14:27:24 +0100

    delete multiple documents backend and frontend

--- api/v1/index.js
+++ api/v1/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Blogpost = require('../models/blogpost');
+const mongoose = require('mongoose');
 
 router.get('/ping', (req, res) => {
     res.status(200).json({msg: 'pong', date: new Date()});
@@ -39,6 +40,7 @@ router.post('/blog-posts', (req, res) => {
 });
 
 router.delete('/blog-posts/:id', (req,res) => {
+	//DELETE: localhost:3000/api/v1/blog-posts/a11aeh565
 	const id = req.params.id;
 	Blogpost.findByIdAndDelete(id, (err, blogPost) => {
 		if(err) {
@@ -47,6 +49,26 @@ router.delete('/blog-posts/:id', (req,res) => {
 		res.status(202).json({ msg: `blog post with id ${blogPost._id} deleted`})
 	});
 	
+});
+
+router.delete('/blog-posts', (req,res) => {
+	//DELETE: localhost:3000/api/v1/blog-posts?ids=a11aeh565, hahf11hff
+	const ids = req.query.ids;
+	console.log('query.ids', ids);
+	const allIds = ids.split(',').map(id => {
+		if(id.match(/^[0-9a-fA-F]{24}$/)){
+			return mongoose.Types.ObjectId((id));
+		} else {
+			console.log('id is not valid', id);
+		}
+	});
+	const condition = { _id: { $in : allIds }}
+	Blogpost.deleteMany(condition, (err, result) => {
+		if(err) {
+			return res.status(500).json(err);
+		}
+		res.status(202).json(result);
+	});
 });
 
 module.exports = router;
